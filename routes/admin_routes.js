5f1ee031513d007b8abb296b2259fe8a2c92d661
Madhumitha <madhumithavenu03@gmail.com>
2024-01-15 15:19:22 +0530

    Login Admin

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addAdmin } = require("../controllers/admin_controller.js");
+const { addAdmin, adminLogin } = require("../controllers/admin_controller.js");
 
 const adminRouter = express.Router();
 
 adminRouter.post("/signup", addAdmin);
+adminRouter.post("/login", adminLogin);
 
 module.exports = adminRouter;
