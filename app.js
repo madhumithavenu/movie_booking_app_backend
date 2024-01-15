7bd6f121947f6c40304c4eab082ebcce6f15d725
Madhumitha <madhumithavenu03@gmail.com>
2024-01-15 15:13:57 +0530

    Sign up admin

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user_routes.js');
+const adminRouter = require('./routes/admin_routes.js');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use("/user",userRouter);
+app.use("/admin",adminRouter);
+
+
 mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.cdpscq1.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(5000, () =>
