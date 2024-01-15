be6cdeb03c270035ba0028ba78a886bf84556fb8
Madhumitha <madhumithavenu03@gmail.com>
2024-01-15 12:40:43 +0530

    connected to mongodb database

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.cdpscq1.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(5000, () =>
+            console.log("Connected to database and server is running ")
+        )
+    })
+    .catch(err => console.log(err))
